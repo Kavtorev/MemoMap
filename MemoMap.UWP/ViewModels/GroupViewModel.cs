2002cd93c7a5e237b7c02bd624d44586c1d7f2e8
Dima Kavtorev <dmitrijkavtorev@gmail.com>
2020-11-27 12:48:33 +0000

    deleted garbage

--- MemoMap.UWP/ViewModels/GroupViewModel.cs
+++ MemoMap.UWP/ViewModels/GroupViewModel.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MemoMap.UWP.ViewModels
 {
-    class GroupViewModel
+    public class GroupViewModel
     {
+        public ObservableCollection<Group> Groups { get; set; }
+
     }
 }
