a4cd1c5975814c8b5dba0f5f540984701b55128c
m305908 <m305908@alunos.ipb.pt>
2020-12-27 16:01:33 +0200

    Close #16

--- MemoMap.UWP/Views/Map/MapPage.xaml.cs
+++ MemoMap.UWP/Views/Map/MapPage.xaml.cs
@@ -12,18 +12,13 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-// library needed for map integration
 using Windows.UI.Xaml.Controls.Maps;
 using Windows.Devices.Geolocation;
 
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace MemoMap.UWP.Views.Map
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MapPage : Page
     {
         public MapPage()
@@ -31,22 +26,15 @@ namespace MemoMap.UWP.Views.Map
             this.InitializeComponent();
         }
 
-        //public void GetPosition()
-        //{
-        //    double lat = Convert.ToDouble(latitude.Text); 
-        //    double longt = Convert.ToDouble(longtitute.Text);
-
-        //    if (lat == null && longt == null) // put here the Notifier that user raise an exception
-        //}
-
         private void MemoMap_MapTapped(MapControl sender, MapInputEventArgs args)
         {
             var GeoPosition = args.Location.Position;
 
-            // generate popup windows with interest point note form(point additional information)
-            // MainPage.NotifyUser(result, NotifyType.StatusMessage);
             latitude.Text = GeoPosition.Latitude.ToString();
             longtitute.Text = GeoPosition.Longitude.ToString();
+
+            // change button visibility
+            AddPoint.IsEnabled = true;
         }
 
         public void Button_Click(object sender, RoutedEventArgs e)
