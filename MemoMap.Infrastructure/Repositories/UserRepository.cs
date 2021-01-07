ba3b30da528f4e7f3c95037043cb4a84dc680b4e
Dmytro Kavtoriev <m305907@alunos.ipb.pt>
2021-01-07 10:27:15 +0000

    UserRepository implementation

--- MemoMap.Infrastructure/Repositories/UserRepository.cs
+++ MemoMap.Infrastructure/Repositories/UserRepository.cs
@@ -1,8 +1,10 @@
 ﻿using MemoMap.Domain;
 using MemoMap.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MemoMap.Infrastructure.Repositories
 {
@@ -12,5 +14,25 @@ namespace MemoMap.Infrastructure.Repositories
         {
 
         }
+
+        public async Task<User> FindByEmailAsync(string email)
+        {
+            return await _dbContext.Users.SingleOrDefaultAsync(m => m.Email == email);
+        }
+
+        public async Task<User> FindByUsernameAndPasswordAsync(string username, string password)
+        {
+            // verify password against hash stored in the db
+            return await _dbContext.Users.SingleOrDefaultAsync(m => m.Username == username &&
+                m.Password == password);
+
+        }
+
+        public async Task<User> FindByUsernameAsync(string username)
+        {
+            return await _dbContext.Users.SingleOrDefaultAsync(m => m.Username == username);
+        }
+
+
     }
 }
