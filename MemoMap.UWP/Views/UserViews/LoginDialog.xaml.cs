bba6462089bcd8464152044cdddc72609771a214
Dmytro Kavtoriev <m305907@alunos.ipb.pt>
2021-01-07 10:29:06 +0000

    Close #25.

---
+++ MemoMap.UWP/Views/UserViews/LoginDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MemoMap.UWP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MemoMap.UWP.Views.UserViews
+{
+    public sealed partial class LoginDialog : ContentDialog
+    {
+        public UserViewModel UserViewModel { get; set; }
+        public LoginDialog()
+        {
+            this.InitializeComponent();
+            UserViewModel = App.UserViewModel;
+
+        }
+
+        private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            await UserViewModel.DoLoginAsync();
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+    }
+}
