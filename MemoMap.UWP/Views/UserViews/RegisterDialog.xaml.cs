aee0535a107770d6484b51fd40c1acfa44aacf37
Dmytro Kavtoriev <m305907@alunos.ipb.pt>
2021-01-08 13:53:52 +0000

    Register Logic. (Client).

--- MemoMap.UWP/Views/UserViews/RegisterDialog.xaml.cs
+++ MemoMap.UWP/Views/UserViews/RegisterDialog.xaml.cs
@@ -25,16 +25,67 @@ namespace MemoMap.UWP.Views.UserViews
         {
             this.InitializeComponent();
             UserViewModel = App.UserViewModel;
-
+     
         }
 
-        private async void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        private async void ContentDialog_Closing(ContentDialog sender, ContentDialogClosingEventArgs args)
         {
-            await UserViewModel.DoRegistrationAsync();
+            // args.Cancel -> true - leave opened dialog
+            // args.Cancel -> false - close dialog 
+            if (args.Result == ContentDialogResult.Primary)
+            {
+                args.Cancel = true;
+
+                // if there are validation errors
+                if (!string.IsNullOrEmpty(UserViewModel.RegistrationFormValidator.ValidateRegistrationForm()))
+                {
+                    UserViewModel.RerenderErrorText(nameof(UserViewModel.RegistrationFormValidator));
+                }
+                else
+                {
+                    // if there are no validation errors then...
+                    // try to login
+                    await UserViewModel.DoRegistrationAsync();
+                    // if there post-validation errors
+                    if (!string.IsNullOrEmpty(UserViewModel.RegistrationFormValidator.Errors))
+                    {
+                        // show post-validation errors
+                        UserViewModel.RerenderErrorText(nameof(UserViewModel.RegistrationFormValidator));
+                    }
+                    else
+                    {
+                        // if data passed all the validations close the dialog
+                        Hide();
+                    }
+                }
+            }
         }
 
         private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
+
         }
+
+        private void UsernameField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UserViewModel.RegistrationFormValidatorSetProperty("username", UsernameField.Text);
+        }
+
+        private void EmailField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UserViewModel.RegistrationFormValidatorSetProperty("email", EmailField.Text);
+        }
+
+        private void PasswordField_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            UserViewModel.RegistrationFormValidatorSetProperty("password", PasswordField.Password);
+        }
+
+        private void RepeatedPasswordField_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            UserViewModel.RegistrationFormValidatorSetProperty("duplicated_password", RepeatedPasswordField.Password);
+        }
+
+        
     }
 }
