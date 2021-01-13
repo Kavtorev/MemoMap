7a38ad90bc885494819f3ffa8090d60e44093a6e
Dmytro Kavtoriev <m305907@alunos.ipb.pt>
2021-01-13 08:53:04 +0000

    Added a base class for the UI rerendering. Close #30.

---
+++ MemoMap.UWP/ViewModels/BindableBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoMap.UWP.ViewModels
+{
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // [CallerMemberName] - attribute which obtains a property name of the caller
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        protected bool SetField<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+
+        }
+    }
+}
