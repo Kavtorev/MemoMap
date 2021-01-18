717df9df2d392cbf53c769cf88662df37d01e7cb
Dmytro Kavtoriev <m305907@alunos.ipb.pt>
2021-01-18 20:52:23 +0000

    Invitational mechanism. Repository.

--- MemoMap.Infrastructure/Repositories/GroupUserRepository.cs
+++ MemoMap.Infrastructure/Repositories/GroupUserRepository.cs
@@ -1,7 +1,9 @@
 ﻿using MemoMap.Domain.Models;
 using MemoMap.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,14 @@ namespace MemoMap.Infrastructure.Repositories
     {
         public GroupUserRepository(MemoMapDbContext db) : base(db)
         {
+        }
+
+
+        public async Task<GroupUser> FindByUserGroupId(int userId, int groupId)
+        {
+            return await _dbContext.GroupUsers
+                .Where(e => e.UserId == userId && e.GroupId == groupId)
+                .SingleOrDefaultAsync();
         }
     }
 }
