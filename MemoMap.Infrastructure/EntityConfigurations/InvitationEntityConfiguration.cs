9cb93cc78acb2433323ff9360713a49ba3e9d6f1
Dmytro Kavtoriev <m305907@alunos.ipb.pt>
2021-01-18 20:50:19 +0000

    Invitation entity configuration.

---
+++ MemoMap.Infrastructure/EntityConfigurations/InvitationEntityConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using MemoMap.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemoMap.Infrastructure.EntityConfigurations
+{
+    public class InvitationEntityConfiguration : IEntityTypeConfiguration<Invitation>
+    {
+        public void Configure(EntityTypeBuilder<Invitation> builder)
+        {
+            builder.Property(p => p.Date).IsRequired();
+
+            //Cannot create a relationship between 'User.Invitations' and 'Invitation.Invited', 
+            //    because there already is a relationship between 
+            //    'User.Invitations' and 'Invitation.Invitor'.Navigation 
+            //    properties can only participate in a single relationship
+
+            builder
+                .HasOne(bc => bc.Group)
+                .WithMany(b => b.Invitations)
+                .HasForeignKey(bc => bc.GroupId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // Invitor
+
+            builder
+                .HasOne(bc => bc.Inviting)
+                .WithMany(c => c.InvitingInvitations)
+                .HasForeignKey(bc => bc.InvitingId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Invited
+
+            builder
+                .HasOne(bc => bc.Invited)
+                .WithMany(c => c.InvitedInvitations)
+                .HasForeignKey(bc => bc.InvitedId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
