f0576dfa91e24df83a32baa719040c732478432b
m305908 <m305908@alunos.ipb.pt>
2021-01-20 19:56:55 +0200

    Ability to save the points to the list, close #50

---
+++ MemoMap.UWP/ViewModels/LocationViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MemoMap.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoMap.UWP.ViewModels
+{
+    public class LocationViewModel
+    {
+        public Location Location { get; set; }
+
+        public LocationViewModel()
+        {
+
+        }
+    }
+}
